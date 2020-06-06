968ad81a53456110ab7d6e2d56ba9c5576afab0e
Tácio Belmonte <taciobelmonte@gmail.com>
2020-06-06 18:01:28 +0200

    Remove unused packages

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -11,8 +11,10 @@ export const Products = styled.div`
   clear: both;
 
   @media (min-width: 767px) {
-    margin: 30px 0 0 0;
     flex-direction: row;
+  }
+  @media (min-width: 992px){
+      margin: 30px 0 0 0;
   }
 `;
 
